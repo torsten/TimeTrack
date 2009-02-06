5a0bb672f8abaacc739515e275d5c952845d3328
Torsten Becker <torsten.becker@gmail.com>
2009-02-06 16:34:24 +0200

    Initial commit.

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TimeTrack
+//
+//  Created by Torsten Becker on 06.02.09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
